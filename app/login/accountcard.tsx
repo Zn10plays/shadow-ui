a2eaee77f475e2c5f6b74eeff26c993f6a849de4
Chase C. <rudrapatel26122004@gmail.com>
2025-07-03 11:32:35 -0400

    minior design changes

--- app/login/accountcard.tsx
+++ app/login/accountcard.tsx
@@ -1,6 +1,7 @@
 import { User } from "@/utils/user"
 import { cookies } from "next/headers"
 import Image from "next/image"
+import Link from "next/link"
 
 async function handleLogout() {
     'use server'
@@ -30,10 +31,17 @@ export default async function AccountCard({user}: AccountCardProps) {
     </div>
 
     <div className="mt-10">
+      <Link href={'/library'} >
+        <button className="mb-1 w-full bg-blue-600 hover:bg-blue-700 text-white font-bold py-3 px-4 rounded-lg focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-900 focus:ring-blue-500 transition duration-300 ease-in-out">
+          Home 
+        </button>
+      </Link>
       <button onClick={handleLogout} className="w-full bg-red-600 hover:bg-red-700 text-white font-bold py-3 px-4 rounded-lg focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-900 focus:ring-red-500 transition duration-300 ease-in-out">
         LogOut 
       </button>
     </div>
+
+    <hr className="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700"></hr>
 
     {/* Footer */}
     <div className="text-center mt-6">
