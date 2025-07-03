406e475a87f293fdca6b83d21c984c313a73619c
Chase C. <rudrapatel26122004@gmail.com>
2025-07-03 11:38:37 -0400

    started library page

---
+++ app/library/page.tsx
@@ -0,0 +1,10 @@
+
+interface LibraryProps {
+
+}
+
+export default function Library (props: LibraryProps) {
+  return <> 
+    <h1 className="text-xl"> You made it to the library, sadly nothing to show... </h1>
+  </>
+}
