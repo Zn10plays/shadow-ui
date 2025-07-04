bd06a0ba84ec7abfba28467cc912bdf814f263f7
Chase C. <rudrapatel26122004@gmail.com>
2025-07-03 21:39:23 -0400

    this is what the previous commit was supposed to do

--- app/library/ShowCaseItem.tsx
+++ app/library/ShowCaseItem.tsx
@@ -12,7 +12,14 @@ interface ShowCaseItemProps {
 export default async function ShowCaseItem({novel}: ShowCaseItemProps) {
   const title = novel.title.includes('|') ? novel.title.split('|')[0] : novel.title;
   const user = await getUser();
-  const is_bookshelfed = await isNovelInBookshelf(novel.id, parseInt(user.id))
+
+  let is_bookshelfed;
+
+  if (!user.is_authenticated) {
+    is_bookshelfed = false
+  } else {
+    is_bookshelfed = await isNovelInBookshelf(novel.id, parseInt(user.id))
+  }
 
   return <div className="w-full">
     <Link href={'/novel/' + novel.id}>
@@ -24,7 +31,7 @@ export default async function ShowCaseItem({novel}: ShowCaseItemProps) {
         <div className="absolute right-0 bg-slate-950 cursor-pointer rounded-bl-md">
         {
           is_bookshelfed ? (
-            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="m438-400 198-198-57-56-141 141-57-57-57 57 114 113ZM200-120v-640q0-33 23.5-56.5T280-840h400q33 0 56.5 23.5T760-760v640L480-240 200-120Zm80-122 200-86 200 86v-518H280v518Zm0-518h400-400Z"/></svg>
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8 md:h-7 md:w-7 lg:h-6 lg:w-6" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="m438-400 198-198-57-56-141 141-57-57-57 57 114 113ZM200-120v-640q0-33 23.5-56.5T280-840h400q33 0 56.5 23.5T760-760v640L480-240 200-120Zm80-122 200-86 200 86v-518H280v518Zm0-518h400-400Z"/></svg>
           ) : (
             <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8 md:h-7 md:w-7 lg:h-6 lg:w-6" viewBox="0 -960 960 960" fill="#e3e3e3"><path d="M200-120v-640q0-33 23.5-56.5T280-840h400q33 0 56.5 23.5T760-760v640L480-240 200-120Zm80-122 200-86 200 86v-518H280v518Zm0-518h400-400Z"/></svg>
           ) 
